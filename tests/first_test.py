bdc2ea2bce102282970c1c449464279e193841b9
Taras Kolodiy <kolodiytaras@gmail.com>
2026-03-15 17:39:13 +0200

    MNFCT-2 add a dotenv library for configurations added dataclass library for simplifications of configs added library faker for random data generation

--- tests/first_test.py
+++ tests/first_test.py
@@ -1,17 +1,63 @@
+import os
+
+import pytest
+from faker import Faker
 from playwright.sync_api import Page, expect
 
+from tests.conftest import Config
 
-def test_login_with_invalid_creds(page: Page):
-    page.goto("https://testomat.io/")
+TARGET_PROJECT = "ZDc"
+
+
+@pytest.fixture(scope="function")
+def login(page: Page, configs: Config):
+    page.goto(configs.login_url)
+    login_user(page, configs.email, configs.password)
+
+
+def test_login_with_invalid_creds(page: Page, configs: Config):
+    open_home_page(page)
 
     expect(page.locator("[href*='sign_in'].login-item")).to_be_visible()
 
     page.get_by_text("Log in", exact=True).click()
+    invalid_password = Faker().password(length=10)
 
-    page.locator("#content-desktop #user_email").fill("taras.testing.2026@gmail.com")
-    page.locator("#content-desktop #user_password").fill("12345")
-    page.get_by_role("button", name="Sign in").click()
+    login_user(page, configs.email, invalid_password)
 
     expect(page.locator("#content-desktop").get_by_text("Invalid Email or password.")).to_be_visible()
     expect(page.locator("#content-desktop .common-flash-info")).to_have_text("Invalid Email or password.")
 
+
+def test_search_project_in_company(page: Page, login):
+    search_for_project(page, TARGET_PROJECT)
+
+    expect(page.get_by_role("heading", name=TARGET_PROJECT)).to_be_visible()
+
+
+def test_should_be_possible_to_open_free_project(page: Page, login):
+    # actr
+    page.locator("#company_id").click()
+    page.locator("#company_id").select_option("Free Projects")
+
+    # assert
+    target_project = "ZDc"
+    search_for_project(page, target_project)
+    expect(page.get_by_role("heading", name=target_project)).to_be_hidden()
+
+    expect(page.get_by_text("You have not created any projects yet")).to_be_visible(timeout=10000)
+
+
+def search_for_project(page: Page, target_project: str):
+    expect(page.get_by_role("searchbox", name="Search")).to_be_visible()
+    page.locator("#content-desktop #search").fill(target_project)
+
+
+def open_home_page(page: Page):
+    page.goto(os.getenv("BASE_URL"))
+
+
+def login_user(page: Page, email, password):
+    page.locator("#content-desktop #user_email").fill(email)
+    page.locator("#content-desktop #user_password").fill(password)
+    page.get_by_role("button", name="Sign in").click()
